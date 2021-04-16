ceb64d9e31584964c31c8db878959e919523213e
Emeric Guédon <emericguedon@orange.fr>
2021-04-16 20:13:07 +0200

    script character WP

---
+++ Script/WpillonSound.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wpillon : MonoBehaviour
+{
+
+    public string FootstepWalk;
+    public string FootstepRun;
+    public string SwordHit;
+    public string SwordWoosh;
+    public string Shield;
+    public string Jump;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Char_FootstepWalk()
+    {
+        AkSoundEngine.SetSwitch("WP_Mooving", "WALK", gameObject);
+        AkSoundEngine.PostEvent(FootstepWalk, gameObject);
+    }
+
+    void Char_FootstepRun()
+    {
+        AkSoundEngine.SetSwitch("WP_Mooving", "RUN", gameObject);
+        AkSoundEngine.PostEvent(FootstepRun, gameObject);
+    }
+
+    void Char_Jump()
+    {
+        AkSoundEngine.PostEvent(Jump, gameObject);
+    }
+
+    void Char_Sword()
+    {
+        AkSoundEngine.PostEvent(SwordWoosh, gameObject);
+    }
+
+    void Char_SwordHit()
+    {
+        AkSoundEngine.SetState("Sword_Materials", "Dry");
+        AkSoundEngine.PostEvent(SwordHit, gameObject);      
+    }
+
+    void Char_Shield()
+    {
+        AkSoundEngine.PostEvent(Shield, gameObject);
+        AkSoundEngine.SetState("Sword_Materials", "Dry");
+        AkSoundEngine.PostEvent(SwordHit, gameObject);
+    }
+}
