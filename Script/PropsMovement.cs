f3887fee9f0557c53bcf4c6ab6a4ff3ca22fb604
Emeric Guédon <emericguedon@orange.fr>
2021-04-17 16:56:21 +0200

    Script PropsMovement

---
+++ Script/PropsMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropsMovement : MonoBehaviour
+{
+    public string RTPC_Name;
+    public string EventName;
+    public Rigidbody rb;
+
+    //[SerializeField] [Range(0f, 1f)]
+    //private float VolumeParameterValue = 0;
+  
+    void Start()
+    {
+        AkSoundEngine.PostEvent(EventName ,gameObject);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AkSoundEngine.SetRTPCValue(RTPC_Name, rb.velocity.z, gameObject);
+    }
+}
